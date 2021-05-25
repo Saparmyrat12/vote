548203c1169d249ba6d856b85f209a03ae3c4f36
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-05-25 14:16:02 +0300

    [1-0004: Create REST to get list of all restaurants] add RestaurantService

---
+++ epam-vote-system-service/src/main/java/com/epam/vote/service/RestaurantService.java
@@ -0,0 +1,25 @@
+/**
+ * <p/>
+ * Copyright (C) 2021
+ * <p/>
+ * Date: май 21, 2021
+ * @author Sapar
+ */
+package com.epam.vote.service;
+
+import com.epam.vote.domain.Restaurant;
+import com.epam.vote.repository.RestaurantDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantDao restaurantDao;
+
+    public List<Restaurant> selectAllRestaurant() {
+        return restaurantDao.selectAllRestaurant();
+    }
+}
