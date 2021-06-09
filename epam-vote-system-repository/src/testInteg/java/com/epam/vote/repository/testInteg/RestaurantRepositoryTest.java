46a4b446c55f68a8f115d26809fe18ff9ef5bf23
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-06-09 14:55:47 +0300

    [1-0004: Create REST to get list of all restaurants] refactoring

--- epam-vote-system-repository/src/testInteg/java/com/epam/vote/repository/testInteg/RestaurantRepositoryTest.java
+++ epam-vote-system-repository/src/testInteg/java/com/epam/vote/repository/testInteg/RestaurantRepositoryTest.java
@@ -31,8 +31,8 @@ public class RestaurantRepositoryTest {
     @Test
     public void findAllRestaurants() {
         List<Restaurant> restaurants = repository.findAllRestaurants();
-        Restaurant restaurant = restaurants.get(0);
         assertEquals(1, restaurants.size());
+        Restaurant restaurant = restaurants.get(0);
         assertEquals("12c1abd4-e9dc-43a0-90aa-441be8e9f8e7", restaurant.getId());
         assertEquals("KFC", restaurant.getName());
         assertEquals("112 Doe Crossing Lane", restaurant.getAddress());
