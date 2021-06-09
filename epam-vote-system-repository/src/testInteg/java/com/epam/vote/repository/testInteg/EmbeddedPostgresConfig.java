b6bf64471a58acd014dc48e1d3b581af2fb52ba7
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-06-09 10:23:47 +0300

    [1-0004: Create REST to get list of all restaurants] add test for repository layer

--- epam-vote-system-repository/src/testInteg/java/com/epam/vote/repository/testInteg/EmbeddedPostgresConfig.java
+++ epam-vote-system-repository/src/testInteg/java/com/epam/vote/repository/testInteg/EmbeddedPostgresConfig.java
@@ -1,24 +1,18 @@
 package com.epam.vote.repository.testInteg;
 
-
-
 import liquibase.integration.spring.SpringLiquibase;
-
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.postgresql.Driver;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-
-import java.io.IOException;
-
-import javax.sql.DataSource;
-
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
+
+import javax.sql.DataSource;
+import java.io.IOException;
 
 /**
  * Configuration for embedded postgres database.
@@ -60,16 +54,17 @@ public class EmbeddedPostgresConfig {
     public DataSource dataSource() {
         return new SimpleDriverDataSource(new Driver(), url);
     }
-/**
- * Change this property to your own that all tests will work
- */
+
+    /**
+     * Change this property to your own that all tests will work
+     */
     @Bean
     public SqlSessionFactoryBean sqlSessionFactory(ApplicationContext applicationContext) throws Exception {
         SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-       // sqlSessionFactory.setMapperLocations(applicationContext.getResources("classpath:com.azarenka.jc.repository.mapper/*.xml"));
-       // sqlSessionFactory.setTypeAliasesPackage("com.azarenka.jc.domain");
+        sqlSessionFactory.setMapperLocations(applicationContext.getResources("classpath:com.epam.vote.system.repository/mapper/*.xml"));
+        sqlSessionFactory.setTypeAliasesPackage("classpath:com.epam.vote.domain");
         sqlSessionFactory.setDataSource(dataSource());
-       // sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis.xml"));
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:com.epam.vote.system.repository/mybatis-config.xml"));
         sqlSessionFactory.afterPropertiesSet();
         return sqlSessionFactory;
     }
---
+++ epam-vote-system-repository/src/testInteg/java/com/epam/vote/repository/testInteg/IRestaurantRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.epam.vote.repository.testInteg;
+
+import com.epam.vote.domain.Restaurant;
+import com.epam.vote.repository.IRestaurantRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test Restaurant repository.
+ * <p/>
+ * Copyright (C) 2021
+ * <p/>
+ * Date: июнь 05, 2021
+ * @author Sapar
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = EmbeddedPostgresConfig.class)
+public class IRestaurantRepositoryTest {
+
+    @Resource
+    private IRestaurantRepository repository;
+
+    @Test
+    public void findAllRestaurants() {
+        List<Restaurant> restaurants = repository.findAllRestaurants();
+        Restaurant restaurant = restaurants.get(0);
+        assertNotNull(restaurants);
+        assertEquals(1, restaurants.size());
+        assertEquals("12c1abd4-e9dc-43a0-90aa-441be8e9f8e7", restaurant.getId());
+        assertEquals("KFC", restaurant.getName());
+        assertEquals("112 Doe Crossing Lane", restaurant.getAddress());
+    }
+}
