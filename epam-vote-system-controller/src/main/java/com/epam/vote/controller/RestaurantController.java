370c846cb4d02323017c7b82d6657f9e83a73c22
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-06-17 18:01:17 +0300

    [1-0005: Create REST to get menu of restaurant] fix bugs

--- epam-vote-system-controller/src/main/java/com/epam/vote/controller/RestaurantController.java
+++ epam-vote-system-controller/src/main/java/com/epam/vote/controller/RestaurantController.java
@@ -1,6 +1,8 @@
 package com.epam.vote.controller;
 
+import com.epam.vote.domain.dto.MenuDto;
 import com.epam.vote.domain.dto.RestaurantDto;
+import com.epam.vote.service.impl.MenuService;
 import com.epam.vote.service.impl.RestaurantService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,12 +32,25 @@ public class RestaurantController {
     @Autowired
     private RestaurantService restaurantService;
 
+    @Autowired
+    private MenuService menuService;
+
     @GetMapping
     public ResponseEntity<List<RestaurantDto>> getAllRestaurants() {
         List<RestaurantDto> restaurantDtoList = restaurantService.getAllRestaurants().stream()
             .map(RestaurantDto::new).collect(Collectors.toList());
         return !restaurantDtoList.isEmpty()
             ? new ResponseEntity<>(restaurantDtoList, HttpStatus.OK)
+            : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/{id}/menu")
+    public ResponseEntity<List<MenuDto>> getMenuOfRestaurant(
+        @PathVariable(name = "id") String id) {
+        List<MenuDto> menuDtoList = menuService.getMenuOfRestaurant(id).stream()
+            .map(MenuDto::new).collect(Collectors.toList());
+        return !menuDtoList.isEmpty()
+            ? new ResponseEntity<>(menuDtoList, HttpStatus.OK)
             : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- epam-vote-system-controller/src/test/java/com/epam/vote/controller/RestaurantControllerTest.java
+++ epam-vote-system-controller/src/test/java/com/epam/vote/controller/RestaurantControllerTest.java
@@ -7,8 +7,11 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.epam.vote.domain.Menu;
 import com.epam.vote.domain.Restaurant;
+import com.epam.vote.domain.dto.MenuDto;
 import com.epam.vote.domain.dto.RestaurantDto;
+import com.epam.vote.service.impl.MenuService;
 import com.epam.vote.service.impl.RestaurantService;
 
 import org.junit.Test;
@@ -18,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,6 +42,9 @@ public class RestaurantControllerTest {
     @Mock
     private RestaurantService restaurantService;
 
+    @Mock
+    private MenuService menuService;
+
     @Test
     public void testGetAllRestaurant() {
         List<Restaurant> expectedRestaurants =
@@ -53,5 +60,22 @@ public class RestaurantControllerTest {
         assertEquals("39c1abd4-e9dc-43a0-90aa-441be8e9f8e7", actualRestaurantDto.getId());
         assertEquals("881 Doe Crossing Lane", actualRestaurantDto.getAddress());
         verify(restaurantService, times(1)).getAllRestaurants();
+    }
+
+    @Test
+    public void testGetMenuOfRestaurant() {
+        List<Menu> expectedMenus = Collections.singletonList(
+            new Menu("550b17ef-389a-47a1-af76-1fb53373da68", "pizza", BigDecimal.valueOf(5.55)));
+        when(menuService.getMenuOfRestaurant("12c1abd4-e9dc-43a0-90aa-441be8e9f8e7")).thenReturn(expectedMenus);
+        ResponseEntity<List<MenuDto>> actualMenuDtoList =
+            restaurantController.getMenuOfRestaurant("12c1abd4-e9dc-43a0-90aa-441be8e9f8e7");
+        assertEquals(1, actualMenuDtoList.getBody().size());
+        MenuDto actualMenuDto = actualMenuDtoList.getBody().get(0);
+        assertTrue(actualMenuDtoList.getStatusCode().is2xxSuccessful());
+        assertNotNull(actualMenuDtoList);
+        assertEquals("550b17ef-389a-47a1-af76-1fb53373da68", actualMenuDto.getId());
+        assertEquals("pizza", actualMenuDto.getName());
+        assertEquals(new BigDecimal("5.55"), actualMenuDto.getPrice());
+        verify(menuService, times(1)).getMenuOfRestaurant("12c1abd4-e9dc-43a0-90aa-441be8e9f8e7");
     }
 }
