791cc5b31b2c4aa4e3bfbdf06f61c598dd2b86d2
Sapar Hudaynazarow <hudaynazarow1515@gmail.com>
2021-07-09 11:04:55 +0300

    [1-0006: Create a new restaurant] added RestExceptionHandler

---
+++ epam-vote-system-controller/src/main/java/com/epam/vote/controller/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.epam.vote.controller.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+/**
+ * Web Security Config.
+ * <p/>
+ * Copyright (C) 2021
+ * <p/>
+ * Date: июль 08, 2021
+ * @author Sapar
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/**").permitAll();
+    }
+
+    @Bean(name = "mvcHandlerMappingIntrospector")
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
+    }
+}
